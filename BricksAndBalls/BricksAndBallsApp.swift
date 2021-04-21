93844f1083ce936317053360af8110712908d92b
Holger Hinzberg <holger@hinzberg.de>
2021-04-21 08:38:32 +0200

    Initial Commit

---
+++ BricksAndBalls/BricksAndBallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BricksAndBallsApp.swift
+//  BricksAndBalls
+//
+//  Created by Holger Hinzberg on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct BricksAndBallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
